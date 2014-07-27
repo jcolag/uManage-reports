003efa054b65f2c188b1e673350a28d6b3ce7cc9
John Colagioia <john@colagioia.net>
2014-07-27 17:38:23 -0400

    Starting point

---
+++ starting.R
@@ -0,0 +1,34 @@
+# Enough to get started
+
+# Set these variables:
+dbName <- "/path/to/your/sqlite.db"
+wxName <- "/path/to/any/weather.csv"
+    # Weather data will end up in the database, eventually
+    # I grab the monthly statistics from
+    # http://www.wunderground.com/history/airport/$airport/$yy/$mm/$dd/MonthlyHistory.html?format=1
+    # with some minor processing
+
+# Use these to manage the data.
+install.packages("data.table")
+install.packages("RSQLite")
+require(data.table)
+require(RSQLite)
+
+# Connect to the database.
+drv <- dbDriver("SQLite")
+dbFile <- dbConnect(drv, dbname = dbName)
+
+# Get the data and use the data tables
+activity <- dbGetQuery(dbFile, "select * from activity")
+activity <- as.data.table(activity)
+weather <- read.csv("weather.csv")
+weather <- as.data.table(weather)
+
+# What do we have?
+summary(weather)
+summary(activity)
+
+# Set the dates as keys, probably needs more work
+setkey(activity, start)
+setkey(weather, Year.Month.Day)
+
