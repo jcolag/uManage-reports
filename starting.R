ea3e18d9ca59b0a8721187576c27651ee538bf00
John Colagioia <john@colagioia.net>
2014-07-30 07:36:20 -0400

    Bridging between tables

--- starting.R
+++ starting.R
@@ -2,11 +2,11 @@
 
 # Set these variables:
 dbName <- "/path/to/your/sqlite.db"
-wxName <- "/path/to/any/weather.csv"
     # Weather data will end up in the database, eventually
-    # I grab the monthly statistics from
+    # uManage grabs the past month's statistics from
     # http://www.wunderground.com/history/airport/$airport/$yy/$mm/$dd/MonthlyHistory.html?format=1
-    # with some minor processing
+    # with some minor pre-processing on (or near) the first of
+    # every month
 
 # Use these to manage the data.
 install.packages("data.table")
@@ -21,16 +21,12 @@ dbFile <- dbConnect(drv, dbname = dbName)
 # Get the data and use the data tables
 activity <- dbGetQuery(dbFile, "select * from activity")
 activity <- as.data.table(activity)
-weather <- read.csv("weather.csv")
+weather <- dbGetQuery(dbFile, "select * from weather")
 weather <- as.data.table(weather)
 
 # What do we have?
 summary(weather)
 summary(activity)
-
-# Set the dates as keys, probably needs more work
-setkey(activity, start)
-setkey(weather, Year.Month.Day)
 
 # Some basic plotting regarding how long I'm using programs in general.
 plot(sort(activity[[4]]))
@@ -38,3 +34,20 @@ plot(sort(log(activity[[4]])))
 
 # Don't forget that the long items may be mostly idle.
 plot(sort(log(activity[[4]] - activity[[5]])))
+
+# Add the date, to make the data comparable to daily data
+activity$date <- substring(activity[[1]], 1, 10)
+
+# Set the dates as keys, probably needs more work
+setkey(activity, date)
+setkey(weather, Year.Month.Day)
+
+# Make sure we have our bearings
+ls()
+
+# What do the tables look like together?
+act_wx <- activity[weather]
+wx_act <- weather[activity]
+summary(act_wx)
+summary(wx_act)
+
